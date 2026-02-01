c582d9bd9175e34fcb343e5ac5a04538e1394f0b
AlexGarbus <alex.garbus@outlook.com>
2026-02-01 16:04:41 -0500

    Credits

--- user_interface/pause/pause.gd
+++ user_interface/pause/pause.gd
@@ -4,13 +4,17 @@ extends Control
 signal fullscreen_toggled(toggled_on: bool)
 signal quit_pressed
 
-@export var _default_button: BaseButton
+@export var _default_main_button: BaseButton
+@export var _default_credits_button: BaseButton
+
+@onready var _main_screen := %MainScreen
+@onready var _credits_screen := %CreditsScreen
 
 
 func _ready() -> void:
 	var fullscreen := DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
 	%FullscreenButton.set_pressed_no_signal(fullscreen)
-	hide()
+	set_visibility(false)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -22,7 +26,10 @@ func set_visibility(value: bool) -> void:
 	visible = value
 	get_tree().paused = value
 	if value:
-		_default_button.call_deferred("grab_focus")
+		_default_main_button.call_deferred("grab_focus")
+	else:
+		_main_screen.show()
+		_credits_screen.hide()
 
 
 func _on_resume_button_pressed() -> void:
@@ -33,5 +40,17 @@ func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
 	fullscreen_toggled.emit(toggled_on)
 
 
+func _on_credits_button_pressed() -> void:
+	_main_screen.hide()
+	_credits_screen.show()
+	_default_credits_button.call_deferred("grab_focus")
+
+
 func _on_quit_button_pressed() -> void:
 	quit_pressed.emit()
+
+
+func _on_credits_back_button_pressed() -> void:
+	_main_screen.show()
+	_credits_screen.hide()
+	_default_main_button.call_deferred("grab_focus")
